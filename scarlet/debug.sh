ea6a5d98a6babce68da4f2672030371957495656
Alexander-Goto <scar.dev.main@gmail.com>
2026-07-13 19:14:20 +0300

    Dev build updated.

--- scarlet/debug.sh
+++ scarlet/debug.sh
@@ -1,2 +1,4 @@
-find src -iname "*.scar" -exec scarlet -n main -m ../std/std.mscar -o scarlet.c -s {} + &&
+export SCAR__FAKE_RND=1
+
+find src -iname "*.scar" -exec scarlet -mt 1 -n main -m ../std/std.mscar -o scarlet.c -s {} + &&
 clang -march=native -pthread -lm -std=c23 -DEXPORT_CORE_BASIC -DEXPORT_CORE_ERROR -DEXPORT_CORE_STRING -DDEBUG -O1 -Wno-empty-body scarlet.c ../std/std.o -o scarlet
