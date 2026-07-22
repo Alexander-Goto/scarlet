35574afed4e92e5fe81bd7cccceb5c8d326d6ff9
Alexander-Goto <scar.dev.main@gmail.com>
2026-07-22 08:38:00 +0300

    Fixed a bug where if an application crashed in one thread, another thread could interrupt the error message and close the application immediately.

--- core/fn/exit.h
+++ core/fn/exit.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include "../common/fn.h"
 #include "../common/include.h"
 #include "../common/macro.h"
 #include "../common/type.h"
 
 [[clang::noinline, noreturn]]
-core t_any McoreFNexit(t_thrd_data* const thrd_data, const t_any*) {exit(EXIT_SUCCESS);}
+core t_any McoreFNexit(t_thrd_data* const thrd_data, const t_any*) {
+     start_failing();
+     exit(EXIT_SUCCESS);
+}
