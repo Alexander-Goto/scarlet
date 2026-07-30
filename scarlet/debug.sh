9f2f48755d3401cbd4b91d32fcdb0877a2acd4e0
Alexander-Goto <scar.dev.main@gmail.com>
2026-07-30 12:19:27 +0300

    Fixed non-working build scripts.

--- scarlet/debug.sh
+++ scarlet/debug.sh
@@ -1,4 +1,6 @@
 export SCAR__FAKE_RND=1
 
-find src -iname "*.scar" -exec scarlet -mt 1 -n main -m ../std/std.mscar -o scarlet.c -s {} + &&
-clang -march=native -pthread -lm -std=c23 -DEXPORT_CORE_BASIC -DEXPORT_CORE_ERROR -DEXPORT_CORE_STRING -DDEBUG -O1 -Wno-empty-body scarlet.c ../std/std.o -o scarlet
+find ../std/src/scar -iname "*.scar" -exec scarlet -mt 1 -n std -o std.mscar -s {} + &&
+clang -march=native -pthread -std=c23 -DDEBUG -O1 -Wno-empty-body -c ../std/src/c/std.c -o std.o &&
+find src -iname "*.scar" -exec scarlet -mt 1 -n main -m std.mscar -o scarlet.c -s {} + &&
+clang -march=native -pthread -lm -std=c23 -DEXPORT_CORE_BASIC -DEXPORT_CORE_ERROR -DEXPORT_CORE_STRING -DDEBUG -O1 -Wno-empty-body scarlet.c std.o -o scarlet
--- scarlet/release.sh
+++ scarlet/release.sh
@@ -1,2 +1,4 @@
-find src -iname "*.scar" -exec scarlet -n main -m ../std/std.mscar -o scarlet.c -s {} + &&
-clang -march=native -pthread -lm -std=c23 -DEXPORT_CORE_BASIC -DEXPORT_CORE_ERROR -DEXPORT_CORE_STRING -O3 -Wno-empty-body scarlet.c ../std/std.o -o scarlet
+find ../std/src/scar -iname "*.scar" -exec scarlet -n std -o std.mscar -s {} + &&
+clang -march=native -pthread -std=c23 -O3 -Wno-empty-body -c ../std/src/c/std.c -o std.o &&
+find src -iname "*.scar" -exec scarlet -n main -m std.mscar -o scarlet.c -s {} + &&
+clang -march=native -pthread -lm -std=c23 -DEXPORT_CORE_BASIC -DEXPORT_CORE_ERROR -DEXPORT_CORE_STRING -O3 -Wno-empty-body scarlet.c std.o -o scarlet
