49863bbd0f74a48cd3767f7884e843e033815ad8
Alexander-Goto <scar.dev.main@gmail.com>
2026-07-30 12:50:10 +0300

    The examples now use the 'std' module from the repository.

--- example/bulls_and_cows/debug.sh
+++ example/bulls_and_cows/debug.sh
@@ -1,4 +1,4 @@
-find std/scar -iname "*.scar" -exec scarlet -n std -o std.mscar -s {} + &&
-clang -march=native -pthread -DDEBUG -std=c23 -O1 -Wno-empty-body -c std/c/std.c -o std.o &&
+find ../../std/src/scar -iname "*.scar" -exec scarlet -mt 1 -n std -o std.mscar -s {} + &&
+clang -march=native -pthread -std=c23 -DDEBUG -O1 -Wno-empty-body -c ../../std/src/c/std.c -o std.o &&
 scarlet -n main -m std.mscar -s game.scar -o game.c &&
 clang -march=native -pthread -lm -DDEBUG -DEXPORT_CORE_BASIC -DEXPORT_CORE_ERROR -DEXPORT_CORE_STRING -std=c23 -O1 -Wno-empty-body std.o game.c -o game
